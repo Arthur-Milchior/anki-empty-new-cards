3d4a5d062e75dfceae12c37b4c8354ca21732d76
Arthur-Milchior <arthur@milchior.fr>
2018-11-21 01:21:02 +0100

    Using same code for 2.0 and 2.1

--- __init__.py
+++ __init__.py
@@ -1,1 +1,11 @@
-from . import emptyNewCards21
+import sys
+if (sys.version_info > (3, 0)):
+     # Python 3 code in this block
+    from . import emptyNewCards21
+else:
+    from . import emptyNewCards20
+
+
+    
+
+from . import 
